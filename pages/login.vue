bb38f8bc8bfb0487b7612245b874eac37f145f83
Richard Rohrig <richard.t.rohrig@gmail.com>
2022-09-10 15:33:03 +0200

    feature(demo) use demo db

--- pages/login.vue
+++ pages/login.vue
@@ -12,7 +12,6 @@ definePageMeta({
   middleware: 'guest'
 })
 
-const config = useRuntimeConfig()
 
 const postLoginForm = async function () {
   await loginWithEmail(email.value, password.value)
@@ -46,7 +45,6 @@ const postLoginForm = async function () {
             </svg>
           </span>
         </div>
-        {{config.private.db}}
         <form v-if="true" v-on:submit.prevent class="mt-8 space-y-6" action="#" method="POST">
           <input type="hidden" name="remember" value="true">
           <div class="rounded-md shadow-sm -space-y-px mb-1">
