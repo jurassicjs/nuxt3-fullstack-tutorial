48dc4e82599b0c5d3adda8a8a761ba9a214cca1e
Richard Rohrig <richard.t.rohrig@gmail.com>
2022-09-24 10:59:38 +0200

    feature(demo) dark mode for text dialog

--- components/elements/TestingPhaseDialog.vue
+++ components/elements/TestingPhaseDialog.vue
@@ -4,13 +4,13 @@
 </script>
 
 <template>
- <div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="false" :hidden="open">
-  <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+ <div class="relative z-10 dark:text-white" aria-labelledby="modal-title" role="dialog" aria-modal="false" :hidden="open">
+  <div class="fixed inset-0 bg-gray-500 bg-opacity-75 dark:bg-opacity-90 transition-opacity"></div>
 
   <div class="fixed inset-0 z-10 overflow-y-auto">
     <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-      <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+      <div class="relative transform overflow-hidden rounded-lg  text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+        <div class="bg-white dark:bg-slate-800 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
           <div class="sm:flex sm:items-start">
             <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
               <svg class="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
@@ -18,14 +18,14 @@
               </svg>
             </div>
             <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-              <h3 class="text-xl font-medium leading-6 text-gray-900" id="modal-title">Test Mode</h3>
+              <h3 class="text-xl font-medium leading-6 text-gray-900 dark:text-white" id="modal-title">Test Mode</h3>
               <div class="mt-2">
-            
+
                 <p class="mt-5">
                  The subscriptions are in TEST MODE
                 </p>
                 <p class="mt-5">
-                 Feel free to play around with it. 
+                 Feel free to play around with it.
                  </p>
                  <p>
                   Any subscriptions you create will be fake.
@@ -37,9 +37,9 @@
             </div>
           </div>
         </div>
-        <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+        <div class="dark:bg-slate-800 bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
           <!-- <button type="button" class="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm">Deactivate</button> -->
-          <button type="button" @click="open = true" class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Okay</button>
+          <button type="button" @click="open = true" class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 dark:bg-slate-800 dark:text-white bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Okay</button>
         </div>
       </div>
     </div>
