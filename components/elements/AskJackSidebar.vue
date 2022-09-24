b142ab062ea7b81ebb41529196e8fae5536394e0
Richard Rohrig <richard.t.rohrig@gmail.com>
2022-09-24 15:59:22 +0200

    feature(demo) add ask sidebar to mobile

--- components/elements/AskJackSidebar.vue
+++ components/elements/AskJackSidebar.vue
@@ -48,8 +48,8 @@ function rehide() {
                 </div>
             </div>
         </dialog-box>
-        <div class="md:w-1/3 pt-5 z-10 boarder flex justify-right fixed">
-            <aside class="w-100 hidden sm:block" aria-label="Sidebar">
+        <div class=" pt-5 z-10 boarder  justify-right sm:fixed">
+            <div class="w-100 sm:block" aria-label="Sidebar">
                 <div class="overflow-y-auto py-4 px-3 bg-gray-50 rounded dark:bg-gray-800">
                     <span @click="goToAskForm" type="button"
                         class="w-full px-6 py-3.5 text-white bg-gradient-to-r from-indigo-500 via-indigo-600 to-indigo-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-indigo-300 dark:focus:ring-indigo-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
@@ -59,12 +59,12 @@ function rehide() {
                         class="w-full px-6 py-3.5 text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
                         Search Questions
                     </nuxt-link>
-                    <button type="button"
+                    <!-- <button type="button"
                         class="w-full px-6 py-3.5 text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
                         Tags
-                    </button>
+                    </button> -->
 
-                    <ul class="space-y-2">
+                    <ul class="space-y-2 hidden sm:block">
                         <li>
                             <a href="#"
                                 class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
@@ -145,7 +145,7 @@ function rehide() {
                         </li>
                     </ul>
                 </div>
-            </aside>
+            </div>
         </div>
     </div>
 
--- components/elements/DialogBox.vue
+++ components/elements/DialogBox.vue
@@ -13,7 +13,7 @@ defineProps({
     :hidden="hide">
     <div class="fixed inset-0 bg-gray-500 bg-opacity-75 dark:bg-opacity-90 transition-opacity"></div>
 
-    <div class="fixed inset-0 z-10 overflow-y-auto">
+    <div class="sm:fixed h-3/4  inset-0 z-10 overflow-y-auto">
       <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
         <div
           class="relative transform overflow-hidden rounded-lg  text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
