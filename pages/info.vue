18ef575fe91bf4f59198ab836c6747f41d21a0f6
Richard <richard.t.rohrig@gmail.com>
2022-09-26 21:52:18 +0200

    feature(deployment) update github action

--- pages/info.vue
+++ pages/info.vue
@@ -2,8 +2,16 @@
 
 const config = useRuntimeConfig()
 
-const releaseVersion = config.public.releaseVersion || 'not available'
+const releaseVersion = config.public.releaseVersion || undefined
 const gitHash = config.public.gitHash || 'not available'
+
+function getReleaseVersion (): String {
+  if(releaseVersion instanceof String) {
+    return releaseVersion.split('refs/tags/')[1]
+  }
+
+  return 'not available'
+}
 
 </script>
  
@@ -31,6 +39,19 @@ const gitHash = config.public.gitHash || 'not available'
               <div class="relative">
                 <dt>
                   <div class="absolute flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
+                    <svg class="h-6 w-6" fill="white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                      <path
+                        d="M320 488c0 9.5-5.6 18.1-14.2 21.9s-18.8 2.3-25.8-4.1l-80-72c-5.1-4.6-7.9-11-7.9-17.8s2.9-13.3 7.9-17.8l80-72c7-6.3 17.2-7.9 25.8-4.1s14.2 12.4 14.2 21.9v40h16c35.3 0 64-28.7 64-64V153.3C371.7 141 352 112.8 352 80c0-44.2 35.8-80 80-80s80 35.8 80 80c0 32.8-19.7 61-48 73.3V320c0 70.7-57.3 128-128 128H320v40zM456 80c0-13.3-10.7-24-24-24s-24 10.7-24 24s10.7 24 24 24s24-10.7 24-24zM192 24c0-9.5 5.6-18.1 14.2-21.9s18.8-2.3 25.8 4.1l80 72c5.1 4.6 7.9 11 7.9 17.8s-2.9 13.3-7.9 17.8l-80 72c-7 6.3-17.2 7.9-25.8 4.1s-14.2-12.4-14.2-21.9V128H176c-35.3 0-64 28.7-64 64V358.7c28.3 12.3 48 40.5 48 73.3c0 44.2-35.8 80-80 80s-80-35.8-80-80c0-32.8 19.7-61 48-73.3V192c0-70.7 57.3-128 128-128h16V24zM56 432c0 13.3 10.7 24 24 24s24-10.7 24-24s-10.7-24-24-24s-24 10.7-24 24z" />
+                    </svg>
+                  </div>
+                  <p class="ml-16 text-lg font-medium leading-6 text-gray-900 dark:text-white">Release Version</p>
+                </dt>
+                <dd class="mt-2 ml-16 text-base text-gray-500 dark:text-white">{{getReleaseVersion()}}</dd>
+              </div>
+
+              <div class="relative">
+                <dt>
+                  <div class="absolute flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
                     <!-- Heroicon name: outline/globe-alt -->
                     <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                       stroke-width="1.5" stroke="currentColor" aria-hidden="true">
@@ -41,22 +62,11 @@ const gitHash = config.public.gitHash || 'not available'
                   <p class="ml-16 text-lg font-medium leading-6 text-gray-900 dark:text-white">Git Hash
                   </p>
                 </dt>
-                <dd class="mt-2 ml-16 text-base text-gray-500 dark:text-white">
-                  {{gitHash}}
-                </dd>
-              </div>
-
-              <div class="relative">
-                <dt>
-                  <div class="absolute flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
-                    <svg class="h-6 w-6" fill="white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                      <path
-                        d="M320 488c0 9.5-5.6 18.1-14.2 21.9s-18.8 2.3-25.8-4.1l-80-72c-5.1-4.6-7.9-11-7.9-17.8s2.9-13.3 7.9-17.8l80-72c7-6.3 17.2-7.9 25.8-4.1s14.2 12.4 14.2 21.9v40h16c35.3 0 64-28.7 64-64V153.3C371.7 141 352 112.8 352 80c0-44.2 35.8-80 80-80s80 35.8 80 80c0 32.8-19.7 61-48 73.3V320c0 70.7-57.3 128-128 128H320v40zM456 80c0-13.3-10.7-24-24-24s-24 10.7-24 24s10.7 24 24 24s24-10.7 24-24zM192 24c0-9.5 5.6-18.1 14.2-21.9s18.8-2.3 25.8 4.1l80 72c5.1 4.6 7.9 11 7.9 17.8s-2.9 13.3-7.9 17.8l-80 72c-7 6.3-17.2 7.9-25.8 4.1s-14.2-12.4-14.2-21.9V128H176c-35.3 0-64 28.7-64 64V358.7c28.3 12.3 48 40.5 48 73.3c0 44.2-35.8 80-80 80s-80-35.8-80-80c0-32.8 19.7-61 48-73.3V192c0-70.7 57.3-128 128-128h16V24zM56 432c0 13.3 10.7 24 24 24s24-10.7 24-24s-10.7-24-24-24s-24 10.7-24 24z" />
-                    </svg>
-                  </div>
-                  <p class="ml-16 text-lg font-medium leading-6 text-gray-900 dark:text-white">Release Version</p>
-                </dt>
-                <dd class="mt-2 ml-16 text-base text-gray-500 dark:text-white">{{releaseVersion}}</dd>
+                <NuxtLink :to="'https://github.com/jurassicjs/nuxt3-fullstack-tutorial/commit/'+gitHash">
+                  <dd class="mt-2 ml-16 text-base text-gray-500 dark:text-white">
+                    {{gitHash}}
+                  </dd>
+                </NuxtLink>
               </div>
             </dl>
           </div>
